f551b754d0a6883cd976044d8cc3e2b4132ea4d8
Emanuele <em.zaccaria@gmail.com>
2020-03-18 16:55:27 +0100

    commenti file js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Chiedi all’utente il suo nome:
+// 1 crea una variabile nome
+// 2 fornisci un input che dia un valore alla variabile nome
+
+// chiedi il suo cognome:
+// 3 crea una variabile cognome
+// 4 fornisci un input che dia un valore alla variabile cognome
+
+
+// chiedi il suo colore preferito:
+// 5 crea una variabile colore
+// 6 fornisci un input che dia un valore alla variabile colore
+
+
+// scrivi sulla pagina il nome cognome e colore preferito + 19
+// utilizza una funzione che faccia comparire in pagina la "password"
